faa77ee6be7b6a6a0c462d8c9f25646d62cb0284
caiogfe <130512314+caiogfe@users.noreply.github.com>
2025-08-02 17:41:38 -0300

    finalizei d007

--- desafios/d007/index.php
+++ desafios/d007/index.php
@@ -9,26 +9,36 @@
 <body>
     <?php
         $salarioMinimo = 1518;
-        $valorSalario = $_GET['salario'];
+        $valorSalario = filter_input(INPUT_GET, 'salario', FILTER_VALIDATE_FLOAT);
         $quantSalario = intdiv($valorSalario, $salarioMinimo);
         $valorRestante = $valorSalario - ($quantSalario * $salarioMinimo);
+        $valorPadrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
     ?>
     <main>
         <h1>Informe seu salário</h1>
         <form action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="get">
             <div class="campo">
                 <label for="idsalario">Salário (R$)</label>
-                <input type="number" name="salario" id="idsalario">
+                <input type="number" name="salario" id="idsalario" step="0.01" value="<?= htmlspecialchars($valorSalario)?>" min="1">
             </div>
             <p>Considerando o salário mínimo de <strong>R$ 1.518,00</strong></p>
             <input type="submit" value="Calcular">
         </form>
     </main>
     <?php 
-        echo "R$ $salarioMinimo<br>";
-        echo "R$ $valorSalario<br>";
-        echo "$quantSalario salários mínimos<br>";
-        echo "R$ $valorRestante<br>";
+        if($valorSalario !== false && $valorSalario > 0): ?>
+            <section>
+                <h2>Resultado Final</h2>
+                <?php 
+                    echo "<p>Quem recebe um salário de " . numfmt_format_currency($valorPadrao, $valorSalario, 'BRL') . " ganha <strong> $quantSalario salários mínimos + " . numfmt_format_currency($valorPadrao, $valorRestante, 'BRL') . "</strong>.</p>";
+                ?>
+            </section>
+        <?php elseif(isset($_GET['salario'])): ?>
+            <section id="erro">
+            <h2>Resultado Final</h2>
+            <?= "<p>Por favor, informe um número válido.</p>" ?>
+            </section>
+        <?php endif;
     ?>
 </body>
 </html>
